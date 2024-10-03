c3c92690d9f4a8f5ecab5fef303f9ccd48c9b473
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2024-10-03 19:36:53 -0300

    layout]

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,15 +1,14 @@
 <nav class="mt-8">
-    <ul class="flex flex-col items-center space-y-1">
+    <ul class="flex-1 flex-col items-center space-y-1">
         <li>
             <!-- Current: "bg-gray-800 text-white", Default: "text-gray-400 hover:text-white hover:bg-gray-800" -->
             <a href="#" class="group flex flex-col items-center justify-center gap-2 rounded-md bg-gray-800 p-2 text-sm font-semibold leading-6 text-white">
-                Icone
+                <x-icons.home />
                 <span>Home</span>
             </a>
         </li>
         <li>
             <a href="#" class="group flex flex-col items-center justify-center gap-2 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
-                Icone
                 <span>Resumos</span>
             </a>
         </li>
