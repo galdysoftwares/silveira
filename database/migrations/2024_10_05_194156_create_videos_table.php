f6bbe393a08c7814bb83973a24c5350e12f92ffa
felipe-silveira-dev <felipe.silveira@eduzz.com.br>
2024-10-05 16:57:54 -0300

    video and summary entities

---
+++ database/migrations/2024_10_05_194156_create_videos_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->string('youtube_id')->unique()->index();
+            $table->string('title');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('videos');
+    }
+};
