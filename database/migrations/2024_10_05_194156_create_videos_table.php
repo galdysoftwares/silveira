8c511be33623ef6025eea2606c9ce67bc1b7c395
felipe-silveira-dev <felipe.silveira@eduzz.com.br>
2024-10-05 22:05:03 -0300

    feature-svr-4: start

--- database/migrations/2024_10_05_194156_create_videos_table.php
+++ database/migrations/2024_10_05_194156_create_videos_table.php
@@ -1,17 +1,16 @@
-<?php
+<?php declare(strict_types = 1);
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         Schema::create('videos', function (Blueprint $table) {
             $table->id();
             $table->string('url');
-            $table->string('youtube_id')->unique()->index();
+            $table->string('youtube_id')->nullable()->unique()->index();
             $table->string('title');
             $table->string('description');
             $table->timestamps();
