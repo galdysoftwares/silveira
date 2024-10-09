b0a1d004907c88cbea6f5f05ca5684bc46c531b1
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2024-10-08 22:40:30 -0300

    feature-svr-3: update layout and github login

---
+++ app/Http/Controllers/Auth/Github/RedirectController.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types = 1);
+
+namespace App\Http\Controllers\Auth\Github;
+
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class RedirectController extends Controller
+{
+    public function __invoke(): RedirectResponse
+    {
+        return Socialite::driver('github')->redirect();
+    }
+}
